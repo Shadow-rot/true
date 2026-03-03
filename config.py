2822c2f021d6ce917a5ae3ec86f4a5ccd1bb0988
Shadwoo <looktouhid@gmail.com>
2026-03-03 11:34:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -43,8 +43,8 @@ class Config:
             if url and "batbin.me" in url
         ]
         self.DEFAULT_THUMB = getenv("DEFAULT_THUMB", "https://te.legra.ph/file/3e40a408286d4eda24191.jpg")
-        self.PING_IMG = getenv("PING_IMG", "https://telegra.ph/SMDBOTZ-03-03")
-        self.START_IMG = getenv("START_IMG", "https://telegra.ph/SMDBOTZ-03-03-2")
+        self.PING_IMG = getenv("PING_IMG", "https://telegra.ph/SMDBOTZ-03-03-2")
+        self.START_IMG = getenv("START_IMG", "https://telegra.ph/SMDBOTZ-03-03-3")
 
     def check(self):
         missing = [
